46f00e2387211a410b4b9ec5f67cf38c6b4560d6
Tyler Freedman <mail@tylerfreedman.com>
2025-08-03 01:37:02 -0400

    Unified MatrixEvent with Hindsight

--- app/models/matrix_event.rb
+++ app/models/matrix_event.rb
@@ -1,10 +1,11 @@
 class MatrixEvent < ActiveRecord::Base
   establish_connection :hindsight
+  belongs_to :room, :primary_key => 'room_id', :class_name => "MatrixRoom"
 
   attr_accessor :offset
 
-  # SocialLink either needs all media on Matrix scraped locally, or it needs keys
-  # to be able to generate media URLs on demand. If you run a homeserver,
+  # SocialLink/Hindsight either need all media on Matrix scraped locally, or they need
+  # keys to be able to generate media URLs on demand. If you run a homeserver,
   # this saves the need to download everything. Normal matrix users that don't
   # run their own homeserver shouldn't use this. This also requires you to run a
   # bridge with the mediaProxy component, which is standard on most bridges. Any of
@@ -16,7 +17,7 @@ class MatrixEvent < ActiveRecord::Base
 
     message = [enddt].pack("G") + mxc.encode("UTF-8")
 
-    keydata = SocialLink::Application.credentials.matrix_media_server_signing_key
+    keydata = Rails.application.credentials.matrix_media_server_signing_key
     key = Base64.urlsafe_decode64(keydata + '=')
     signature = OpenSSL::HMAC.digest("SHA512", key, message)
 
@@ -25,6 +26,6 @@ class MatrixEvent < ActiveRecord::Base
   end
 
   def timestamp
-    return (self.origin_server_ts / 1000).to_i + (self.offset || 0)
+    return Time.at((self.origin_server_ts + (self.offset || 0)) / 1000.to_f)
   end
 end
