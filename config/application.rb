85f4b7f8e08d3333c92bf1f1befc220558cc409e
Tyler Freedman <mail@tylerfreedman.com>
2025-08-15 15:46:16 -0400

    Dropped unnecessary rails components

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,25 @@
 require_relative "boot"
 
-require "rails/all"
+
+require "rails"
+
+# Pick the frameworks you want:
+# require "action_cable/engine"
+# require "action_mailbox/engine"
+# require "action_mailer/railtie"
+
+# require "action_text/engine"
+require "action_view/railtie"
+
+# require "active_job/railtie"
+require "active_model/railtie"
+require "active_record/railtie"
+
+# require "active_storage/engine"
+
+require "action_controller/railtie"
+# require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
